f25789e7c3ad3d46499a2a27f74a466439f0c0fa
tarashurik@mail.ru <tarashurik@mail.ru>
2022-04-20 10:32:06 +0300

    add authentication to upload files

--- filesstoragedjango/app_files/views.py
+++ filesstoragedjango/app_files/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets, permissions
 from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication
 
 from app_files.serializers import FileUploadSerializer
 from app_files.models import FileUpload
@@ -9,7 +10,8 @@ class FileUploadViewSet(viewsets.ModelViewSet):
     queryset = FileUpload.objects.all()
     serializer_class = FileUploadSerializer
     parser_classes = (MultiPartParser, FormParser,)
-    # permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [BasicAuthentication, SessionAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
 
     def perform_create(self, serializer):
         file = self.request.data.get('file')
