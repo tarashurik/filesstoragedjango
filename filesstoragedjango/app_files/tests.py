f3c027b818f34d2858d3747ecbd327949015d1dd
tarashurik@mail.ru <tarashurik@mail.ru>
2022-05-05 11:07:39 +0300

    refactor: move register and login admin_user from each test to setUp in FilesTest

--- filesstoragedjango/app_files/tests.py
+++ filesstoragedjango/app_files/tests.py
@@ -1,6 +1,5 @@
 import os
 
-from django.contrib.auth.models import User
 from django.test import TestCase
 from filesstoragedjango.settings import BASE_DIR
 
@@ -19,24 +18,17 @@ user_data = {
 
 
 class FilesTest(TestCase):
-    def register_admin(self):
+    def setUp(self):
         self.client.post(register_url, data=user_data)
-        user = User.objects.all()[0]
-        return user
-
-    def test_simple_file_upload(self):
-        self.register_admin()
         self.client.login(username=user_data.get('username'), password=user_data.get('password'))
 
+    def test_simple_file_upload(self):
         with open(simple_file_dir, 'rb') as file:
             data = {'file': file}
             response = self.client.post(file_url, data=data)
         self.assertEqual(response.status_code, 201)
 
     def test_simple_file_copy_upload(self):
-        self.register_admin()
-        self.client.login(username=user_data.get('username'), password=user_data.get('password'))
-
         with open(simple_file_dir, 'rb') as file:
             data = {'file': file}
             self.client.post(file_url, data=data)
@@ -48,9 +40,6 @@ class FilesTest(TestCase):
         self.assertTrue('You have already have File with same content' in str(response.content))
 
     def test_too_big_file_upload(self):
-        self.register_admin()
-        self.client.login(username=user_data.get('username'), password=user_data.get('password'))
-
         with open(too_big_file_dir, 'rb') as file_too_big:
             data = {'file': file_too_big}
             response = self.client.post(file_url, data=data)
