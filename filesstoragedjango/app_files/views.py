646bb21e501c963750589cb758e5a7e3ad29e9fd
tarashurik@mail.ru <tarashurik@mail.ru>
2022-05-11 08:47:54 +0300

    refactor: delete unusefull permission

--- filesstoragedjango/app_files/views.py
+++ filesstoragedjango/app_files/views.py
@@ -4,14 +4,13 @@ from rest_framework.authentication import SessionAuthentication
 
 from app_files.serializers import FileUploadSerializer
 from app_files.models import FileUpload
-from app_files.permissions import IsAuthor
 
 
 class FileUploadViewSet(viewsets.ModelViewSet):
     serializer_class = FileUploadSerializer
     parser_classes = (MultiPartParser, FormParser,)
     authentication_classes = [SessionAuthentication]
-    permission_classes = [permissions.IsAuthenticated, IsAuthor]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         queryset = FileUpload.objects.filter(owner=self.request.user)
